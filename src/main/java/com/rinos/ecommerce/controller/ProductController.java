1e0bb1cd0491d7b9fbf55855859377d98e8f31d0
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-30 00:06:21 +0530

    ProductController created

---
+++ src/main/java/com/rinos/ecommerce/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.rinos.ecommerce.controller;
+
+import com.rinos.ecommerce.entity.Product;
+import com.rinos.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products); // to return products as JSON response
+    }
+}
