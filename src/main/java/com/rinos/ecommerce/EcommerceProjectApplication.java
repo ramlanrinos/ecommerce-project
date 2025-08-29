6db0595b55bf2838b515413f6808ed99d06db5de
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-30 00:26:02 +0530

    Block the Spring Boot Security temporary

--- src/main/java/com/rinos/ecommerce/EcommerceProjectApplication.java
+++ src/main/java/com/rinos/ecommerce/EcommerceProjectApplication.java
@@ -2,8 +2,9 @@ package com.rinos.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // for block the security service temporary
 public class EcommerceProjectApplication {
 
     public static void main(String[] args) {
