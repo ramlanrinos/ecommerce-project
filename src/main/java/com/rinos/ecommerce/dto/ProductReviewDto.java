890909c59a7c3f2a6f781e34df2f9b819ced8096
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-30 23:26:58 +0530

    ProductReviewDto class created

---
+++ src/main/java/com/rinos/ecommerce/dto/ProductReviewDto.java
@@ -0,0 +1,34 @@
+package com.rinos.ecommerce.dto;
+
+public class ProductReviewDto {
+    private Long productId;
+    private String comment;
+    private Double rating;
+
+    public ProductReviewDto() {
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+}
